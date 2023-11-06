526afb4701af258381869a58b1a97d13650af3f1
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2023-11-06 16:13:06 -0300

    feat: Adicionando busca de Medicamentos

--- src/pages/MedicineList/index.jsx
+++ src/pages/MedicineList/index.jsx
@@ -1,13 +1,20 @@
 import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
-import { Card, CardContent, Button } from "@mui/material"
+import { Card, CardContent, Button, TextField } from "@mui/material"
 import { Link } from "react-router-dom"
 import { MedicineCard } from "../../components/MedicineCard"
 import { useApp } from "../../hooks/useApp"
+import { useMemo, useState } from "react"
 import "./style.css"
 
 function MedicineList() {
   const { listMedicines } = useApp()
+  const [ busca, setBusca ] = useState()
+
+  const listMedicinesFilter = useMemo(() => {
+    return (busca == undefined || busca == '') ? listMedicines :  
+      listMedicines.filter((medicine) => medicine.medicamento.toLowerCase().includes(busca.toLowerCase()))
+  }, [busca])
 
   return (
     <>
@@ -30,11 +37,23 @@ function MedicineList() {
             </Link>
             <div id="titulo-list-medicamentos">
               <h2>Listagem de Medicamentos</h2>
+              <TextField
+                  sx={{ width: '97%' }}
+                  color="success"
+                  margin="normal"
+                  label="Buscar Medicamento"
+                  id="busca"
+                  name="busca"
+                  type="text"
+                  value={busca || ''}
+                  onChange={(event) => setBusca(event.target.value)}
+                />
             </div>
-            {listMedicines?.length > 0 ?
+
+            {listMedicinesFilter?.length > 0 ?
               <div id="medicines-list">
                 {
-                  listMedicines.map((medicine) => (
+                  listMedicinesFilter.map((medicine) => (
                     <MedicineCard key={medicine.id} medicine={medicine} />
                   ))
                 }
